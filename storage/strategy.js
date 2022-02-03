974a5b35694d59975c16d1cbfa26768eceb44f16
Souvik Das <souvik@jubi.ai>
2022-02-03 23:13:05 +0530

    [refactor] test in heroku

--- storage/strategy.js
+++ storage/strategy.js
@@ -28,9 +28,10 @@ function get(){
                 try{
                     if(data){
                         data=JSON.parse(data)
+                        
                         Object.keys(process.env).forEach((key)=>{
                             try{
-                                console.log(keys)
+                                console.log(key)
                                 if(key.startsWith("STRATEGY__")&&key.endsWith("__QTY")){
                                     const [_,strategyName,brokerName,type]=key.split("__")
                                     if(type=="QTY"){
