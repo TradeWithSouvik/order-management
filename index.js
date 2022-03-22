d57e0c287db09c557684e693b266436d06ee2926
souvik131 <souvik131@gmail.com>
2022-03-22 10:35:33 +0530

    [fix] path issue

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ function getDir() {
     if (process.pkg) {
         return path.resolve(process.execPath + "/..");
     } else {
-        return path.join(require.main ? require.main.path : process.cwd());
+        return path.join(require.main&&require.main.path ? require.main.path : process.cwd());
     }
 }
 
