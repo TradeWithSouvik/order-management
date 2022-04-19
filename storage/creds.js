f8f1ef42f1c879f620032229355cac3051a4b633
souvik131 <souvik131@gmail.com>
2022-04-19 13:39:13 +0530

    [refactor] creds from env file

--- storage/creds.js
+++ storage/creds.js
@@ -43,43 +43,44 @@ let storedData
 function set(data){
     return new Promise((resolve,reject)=>{
         storedData=data
-
-        for(const key of Object.keys(data)){
-            if(data[key]==undefined){
-                delete data[key]
-            }
-            process.env[key]=data[key]
-        }
         resolve(storedData)
-        fs.writeFile(`${process.cwd()}/data/creds.json`, JSON.stringify(data), function (err) {
-            if (err) {
-                console.log(err)
-            }
+        // for(const key of Object.keys(data)){
+        //     if(data[key]==undefined){
+        //         delete data[key]
+        //     }
+        //     process.env[key]=data[key]
+        // }
+        // resolve(storedData)
+        // fs.writeFile(`${process.cwd()}/data/creds.json`, JSON.stringify(data), function (err) {
+        //     if (err) {
+        //         console.log(err)
+        //     }
                       
-        });
+        // });
     })
     
 }
 
 function get(){
     return new Promise((resolve,reject)=>{
-        if(storedData){
-            return resolve(storedData)
-        }
-        else{
-            fs.readFile(`${process.cwd()}/data/creds.json`, 'utf8', function(err, data){
+        resolve(process.env)
+        // if(storedData){
+        //     return resolve(storedData)
+        // }
+        // else{
+        //     fs.readFile(`${process.cwd()}/data/creds.json`, 'utf8', function(err, data){
                 
-                try{
-                    if(data){
-                        return resolve(JSON.parse(data));
-                    }
-                }
-                catch(e){
-                    console.log(e)
-                }
-                return resolve({});
-            });
-        }
+        //         try{
+        //             if(data){
+        //                 return resolve(JSON.parse(data));
+        //             }
+        //         }
+        //         catch(e){
+        //             console.log(e)
+        //         }
+        //         return resolve({});
+        //     });
+        // }
     });
 }
 
